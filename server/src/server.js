6e546f00e184a6d73fd43865d5e1f77876369b11
Jose Gamboa <joalgamro2010@gmail.com>
2021-11-17 16:17:22 -0600

    C: server

---
+++ server/src/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv/config');
+
+// Middleware
+app.use(cors());
+
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+);
+app.use(express.json());
+
+// imports ROUTES
+
+const userRoute = require('./routes/drive');
+
+app.use('/drive', userRoute);
+
+mongoose.connect(
+    process.env.DB_CONNECTION, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    },
+    () => {
+        console.log('connected to WebDrive DB!!!');
+    }
+);
+
+// server listing in  port 3000
+app.listen(3000);
