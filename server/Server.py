f9912807537b2b41a6e590c1fb8ff428b58119ec
Jose Gamboa <joalgamro2010@gmail.com>
2021-11-20 14:34:50 -0600

    A: session routes

--- server/Server.py
+++ server/Server.py
@@ -6,7 +6,34 @@ app = Flask(__name__)
 fileSystem = FileSystem()
 
 
+#Session Routes
+@app.route('/users', methods =['POST'])
+def postUser():
+    user = request.json.get("user")
+    maxBytes = request.json.get("maxBytes")
+    return fileSystem.signUp(user, maxBytes)
 
+@app.route('/users', methods =['GET'])
+def getUsersList():
+    user = request.json.get("user")
+    return {"sesions":fileSystem.sessions, "drives":fileSystem.drives}
+
+@app.route('/users/paths', methods =['GET'])
+def getUsersPaths():
+    user = request.json.get("user")
+    return fileSystem.getCurrentPaths(user)
+    
+
+@app.route('/users/logIn', methods =['POST'])
+def postLogIn():
+    user = request.json.get("user")
+    return fileSystem.logIn(user)
+
+
+@app.route('/users/logOut', methods =['POST'])
+def postLogout():
+    user = request.json.get("user")
+    return fileSystem.logOut(user)
 
 #Main
 if __name__ == "__main__":
