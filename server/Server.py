d25374817106d1a4b34de22cd496538c0f647e33
Jose Gamboa <joalgamro2010@gmail.com>
2021-11-20 14:37:19 -0600

    A: folders routes

--- server/Server.py
+++ server/Server.py
@@ -35,6 +35,46 @@ def postLogout():
     user = request.json.get("user")
     return fileSystem.logOut(user)
 
+
+#FileSystem Routes
+
+#Folders Routes 
+@app.route('/folders/current', methods =['GET'])
+def getCurrentFolder():
+    user = request.json.get("user")
+    return fileSystem.getCurrentFolder(user)
+
+@app.route('/folders', methods =['POST'])
+def postFolder():
+    user = request.json.get("user")
+    name = request.json.get("name")
+    return fileSystem.createFolder(user, name)
+
+@app.route('/folders', methods =['DELETE'])
+def deleteFolder():
+    user = request.json.get("user")
+    name = request.json.get("name")
+    return fileSystem.delete(user, name)
+
+@app.route('/folders', methods =['PATCH'])
+def updateFolder():
+    user = request.json.get("user")
+    name = request.json.get("name")
+    newName = request.json.get("newName")
+    return fileSystem.updateFolder(user, name, newName)
+
+@app.route('/folders/open', methods =['POST'])
+def postOpenFolder():
+    user = request.json.get("user")
+    name = request.json.get("name")
+    return fileSystem.openFolder(user, name)
+
+@app.route('/folders/close', methods =['POST'])
+def postCloseFolder():
+    user = request.json.get("user")
+    return fileSystem.closeFolder(user)
+
+
 #Main
 if __name__ == "__main__":
     app.run(host="localhost", port=8000, debug = True)
