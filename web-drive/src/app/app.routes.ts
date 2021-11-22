5b1144001fa80dd9f4575a4e39b0af6cfec07c30
Melania Morales <melaniacrc@gmail.com>
2021-11-22 13:55:14 -0600

    U: Bugs

--- web-drive/src/app/app.routes.ts
+++ web-drive/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./components/home/home.component";
 import { LoginComponent } from "./components/users/login/login.component";
 import { SignUpComponent } from "./components/users/sign-up/sign-up.component";
 
@@ -6,6 +7,7 @@ import { SignUpComponent } from "./components/users/sign-up/sign-up.component";
 const APP_ROUTES : Routes = [
     {path: 'login', component: LoginComponent},
     {path: 'sign-up', component: SignUpComponent},
+    {path: 'home', component: HomeComponent},
     {path: '**', pathMatch: 'full', redirectTo: 'login'} /* PREDETERMINADA */
 ];
 
