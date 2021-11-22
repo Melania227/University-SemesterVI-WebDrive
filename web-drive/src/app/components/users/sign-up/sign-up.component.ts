6dbe30cb2814c4ffab5b6ca1eee9d0bfdc7621ec
Melania Morales <melaniacrc@gmail.com>
2021-11-22 11:27:59 -0600

    C: Sign up component

---
+++ web-drive/src/app/components/users/sign-up/sign-up.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { CustomResponse } from 'src/app/models/custom-response.model';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.scss']
+})
+export class SignUpComponent implements OnInit {
+  form!: FormGroup;
+
+  constructor(
+    private readonly _fb: FormBuilder,
+    private readonly _userService: UserService,
+    private _snackBar: MatSnackBar,
+    private readonly _router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  get usernameInvalid(){
+    return this.form.get('username')?.invalid && this.form.get('username')?.touched
+  }
+  get maxBytesInvalid(){
+    return this.form.get('username')?.invalid && this.form.get('username')?.touched
+  }
+
+  createForm(){
+    this.form = this._fb.group(
+      {
+        username: ['', [Validators.required]],
+        maxBytes: ['', [Validators.required, Validators.pattern(/\d+/)]]
+      }
+    )
+  }
+
+  async signUp(){
+    if (this.form.valid){
+      let res = (await this._userService.createUser(this.form.get("username")?.value, this.form.get("maxBytes")?.value).toPromise());
+      if(res.error){
+        console.log(res.response);
+        this._snackBar.open(res.response, "Ok", {
+          duration: 3000,
+          panelClass: ['custom-class'],
+        });
+      }
+      else{
+        //this._router.navigateByUrl(`invoices-list`);
+        console.log(res.response);
+      }
+    }
+  }
+
+}
